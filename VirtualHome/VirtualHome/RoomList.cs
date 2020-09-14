c5cf10d85ef61006cfd1d9d9682e5c28d5e9a725
LukaszGazda <LukaszGazda@gds.ey.com>
2020-09-14 21:07:21 +0200

    add RoomList

---
+++ VirtualHome/VirtualHome/RoomList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualHome
+{
+    public enum RoomList
+    {
+        Kitchen = 1,
+        Bathroom = 2,
+    }
+}
