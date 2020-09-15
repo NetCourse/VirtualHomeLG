bd0d96f30e2db8faff4b94dcbbaf4cb639948cee
LukaszGazda <LukaszGazda@gds.ey.com>
2020-09-15 08:46:08 +0200

    add device and room class

---
+++ VirtualHome/VirtualHome/Device.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualHome
+{
+    class Device
+    {
+        private string name;
+
+        public Device(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string Name { get => name; set => name = value; }
+
+    }
+}
---
+++ VirtualHome/VirtualHome/Room.cs
@@ -0,0 +1,17 @@
+﻿
+namespace VirtualHome
+{
+    class Room
+    {
+        private string name;
+        private string type;
+
+        public Room(string Name)
+        {
+            this.name = Name;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+    }
+}
